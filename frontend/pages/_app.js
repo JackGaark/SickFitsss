216e5f1bba41bc24ce482f7dbd51302f90b16ffb
JackGaark <37947425+JackGaark@users.noreply.github.com>
2022-06-20 18:02:50 -0400

    new project

---
+++ frontend/pages/_app.js
@@ -0,0 +1,41 @@
+import App, { Container } from "next/app";
+import { ApolloProvider } from "react-apollo";
+import Page from "../components/Page";
+import withData from "../lib/withData";
+import NProgress from "nprogress";
+import Router from "next/router";
+import "../components/styles/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.start());
+Router.events.on("routeChangeError", () => NProgress.start());
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return {
+      pageProps,
+    };
+  }
+
+  render() {
+    const { Component, apollo, pageProps } = this.props;
+
+    return (
+      <Container>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
+      </Container>
+    );
+  }
+}
+
+export default withData(MyApp);
